43e7af8d5ff79672071149c82408e4910d810143
Milbert Barcelo <milbert.barcelo@my.marianas.edu>
2022-11-30 22:25:38 +1000

    first commit

---
+++ javascript/signup.js
@@ -0,0 +1,8 @@
+function pcsh1() {
+    var x = document.getElementById("pc1");
+    if (x.classList.contains("hide")) {
+      x.classList.remove("hide");
+    } else {
+      x.classList.add("hide");
+    }
+  }
